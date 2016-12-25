6ea4206dccbde195c6327069a0276b83202f2287
ko <entengelb@gmail.com>
2016-12-25 12:50:13 +0100

    Added toucpad toggle scripts

---
+++ roles/thinkpad/files/touchpad_status.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+declare -i ID
+ID=`xinput list | grep -Eio '(touchpad|glidepoint)\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+declare -i STATE
+xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'
