571ed424c857b7b6fc87b6c6077c33ab364bddbe
ko <entengelb@gmail.com>
2016-12-28 23:45:26 +0100

    Added autorun things

---
+++ nest.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+LOGDIR="`dirname $0`/logs"
+mkdir -p ${LOGDIR}
+ansible-playbook -i inventory/real/hosts site.yml > "${LOGDIR}/`date "+%Y-%m-%d_%H-%M-%S"`.log"
+find ${LOGDIR} -mtime +30 -type f -delete
