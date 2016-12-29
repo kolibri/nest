a2218bdf10b1011e376e7e7bf48ff1275d226024
ko <entengelb@gmail.com>
2016-12-29 02:07:02 +0100

    nest via systemd

--- nest.sh
+++ nest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
 LOGDIR="`dirname $0`/logs"
+cd `dirname $0`
+
 mkdir -p ${LOGDIR}
 ansible-playbook -i inventory/real/hosts site.yml > "${LOGDIR}/`date "+%Y-%m-%d_%H-%M-%S"`.log"
 find ${LOGDIR} -mtime +30 -type f -delete
